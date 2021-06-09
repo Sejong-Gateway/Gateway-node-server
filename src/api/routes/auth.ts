0d3746d26e1384650dff6b7cf0be1fd889a4d049
eunsolkang <eunsol2953@gmail.com>
2021-06-09 19:59:33 +0900

    Create auth route

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,7 +1,22 @@
-import {Router} from 'express';
+import {Request, Response, Router} from 'express';
+import {Container} from "typedi";
+import CollageService from "../../services/CollageService";
+import UserService from "../../services/UserService";
 
 const route = Router();
 
 export default(app: Router) =>{
-    
+    app.use('/auth', route);
+
+    route.post('/signup', async(req: Request, res: Response)=>{
+        const userServiceInstance = Container.get(UserService);
+        const {user, token} = await userServiceInstance.signUp(req.body);
+
+        return res.json({
+            user: user,
+            token: token
+        });
+    });
+
+
 }
