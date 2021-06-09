22b0566333ea51e17cd0ea003dbb27c6b530cf4d
eunsolkang <eunsol2953@gmail.com>
2021-06-09 20:00:10 +0900

    Add route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
 import user from './routes/user';
+import collage from "./routes/collage";
+import department from "./routes/department";
+import subject from "./routes/subject";
+import auth from "./routes/auth";
+import category from "./routes/category";
+import requirement from "./routes/requirement";
 
 export default () => {
 	const app = Router();
 	user(app);
-
+	collage(app);
+	department(app);
+	subject(app);
+	auth(app);
+	category(app);
+	requirement(app);
+	
 	return app
 }
