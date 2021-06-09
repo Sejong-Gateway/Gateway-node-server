b44a4ed850d7dde6a198cc5929b66bf2656d32c6
eunsolkang <eunsol2953@gmail.com>
2021-06-09 19:59:50 +0900

    Create BaseService

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -1,18 +1,50 @@
-import { getConnection, Repository } from "typeorm";
-import { BaseModel } from "../models/BaseModel";
-export type ObjectType<T> = { new (): T } | Function;
+import {DeleteResult, getConnection, Repository} from "typeorm";
+import {BaseModel} from "../models/BaseModel";
 
-export abstract class BaseService<T extends BaseModel>{
+export type ObjectType<T> = { new(): T } | Function;
+
+export abstract class BaseService<T extends BaseModel> {
     protected genericRepository: Repository<T>;
     private repo: ObjectType<T>;
-    
+
     constructor(repo: ObjectType<T>) {
         this.genericRepository = getConnection().getRepository(repo);
         this.repo = repo;
     }
 
-    async list(relations?: string[]){
+    async list(relations?: string[]) {
         const list = this.genericRepository.find({
+            relations
+        }) as Promise<T[]>;
+
+        return list;
+    }
+
+    async getByUUID(uuid: string, relations?: string[]): Promise<T> {
+        return this.genericRepository.findOne({
+            where: {uuid},
+            relations,
+        }) as Promise<T>;
+    }
+
+    async softDelete(uuid: string): Promise<T> {
+        const oldOne = await (this.getByUUID(uuid) as Promise<T>);
+        const newOne: Partial<T> = {};
+        newOne.deletedAt = new Date();
+        return this.genericRepository.save({...oldOne, ...newOne} as object);
+    }
+
+    async hardDelete(uuid: string): Promise<DeleteResult> {
+        return getConnection()
+            .createQueryBuilder()
+            .delete()
+            .from(this.repo)
+            .where('uuid = :uuid', {uuid})
+            .execute();
+    }
+
+    async getByUUIDs(uuids: string[], relations?: string[]) {
+        const list = this.genericRepository.findByIds(uuids, {
             relations,
         }) as Promise<T[]>;
 
